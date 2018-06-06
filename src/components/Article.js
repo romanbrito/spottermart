df4ecc0264aca62767ad8a6b1cef1fa567e3b62e
Roman Brito <romanbrito1@gmail.com>
2018-06-05 19:01:09 -0500

    layout

---
+++ src/components/Article.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Featured from './Featured'
+import ArticleList from './ArticleList'
+
+const Article = () =>
+  <article>
+    <Featured/>
+    <ArticleList/>
+  </article>
+
+export default Article
--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Hero from './Hero'
+import Article from './Article'
 
 const FrontPage = () =>
   <div>
     <Hero/>
+    <Article/>
   </div>
 
 export default FrontPage
