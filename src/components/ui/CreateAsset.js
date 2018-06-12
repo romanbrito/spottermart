4d427fec38008aa96b33253981b9b61d3586b4ae
roman <romanbrito1@gmail.com>
2018-06-11 23:09:57 -0500

    maping inputs to create asset

--- src/components/ui/CreateAsset.js
+++ src/components/ui/CreateAsset.js
@@ -1,42 +1,53 @@
 import React from 'react'
 import styled from "styled-components"
+import formInput from '../../json/form_input.json'
 
-const CreateAsset = () =>
-    <div className="measure pa4 black-80">
-      <Label htmlFor="businessName">Business Name <Span>(optional)</Span></Label>
-      <Input id="businessName" type="text" aria-describedby="businessName-desc"/>
+const CreateAsset = () => (
+  <div>
+    <section>
+      <h3>Section 1</h3>
+        <ul>
+          {formInput.section1.map(formInput =>
+          <li>
+            <label htmlFor={formInput.labelFor}>{formInput.label}</label>
+            <input
+              type={formInput.type}
+              id={formInput.id}
+              name={formInput.name}/>
+          </li>
+          )}
+        </ul>
+    </section>
+    <section>
+      <h3>Section 2</h3>
+      <ul>
+        {formInput.section2.map(formInput =>
+          <li>
+            <label htmlFor={formInput.labelFor}>{formInput.label}</label>
+            <input
+              type={formInput.type}
+              id={formInput.id}
+              name={formInput.name}/>
+          </li>
+        )}
+      </ul>
+    </section>
+    <section>
+      <h3>Section 3</h3>
+      <ul>
+        {formInput.section2.map(formInput =>
+          <li>
+            <label htmlFor={formInput.labelFor}>{formInput.label}</label>
+            <input
+              type={formInput.type}
+              id={formInput.id}
+              name={formInput.name}/>
+          </li>
+        )}
+      </ul>
+    </section>
 
-      <Label htmlFor="description">Description <span className="normal black-60">(optional)</span></Label>
-      <Input id="description" type="text" aria-describedby="description-desc"/>
-
-      <Label htmlFor="price">Price <Span>(optional)</Span></Label>
-      <Input id="price" type="text" aria-describedby="name-desc"/>
-
-      <Label htmlFor="businessType">Business Type <Span>(optional)</Span></Label>
-      <Input id="businessType" type="text" aria-describedby="businessType-desc"/>
-
-      <Label htmlFor="address">Address <Span>(optional)</Span></Label>
-      <Input id="address" type="text" aria-describedby="address-desc"/>
-
-      <Label htmlFor="city">City <Span>(optional)</Span></Label>
-      <Input id="city" type="text" aria-describedby="city-desc"/>
-
-      <Label htmlFor="state">State <Span>(optional)</Span></Label>
-      <Input id="state" type="text" aria-describedby="state-desc"/>
-
-      <Label htmlFor="zipCode">Zip Code <Span>(optional)</Span></Label>
-      <Input id="zipCode" type="text" aria-describedby="zipCode-desc"/>
-
-    </div>
+  </div>
+)
 
 export default CreateAsset
-
-const Label = styled.label.attrs({
-  className: "f6 b db mb2"
-})``
-const Span = styled.span.attrs({
-  className:"normal black-60"
-})``
-const Input = styled.input.attrs({
-  className:"input-reset ba b--black-20 pa2 mb2 db w-100"
-})``
