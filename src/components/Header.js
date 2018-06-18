a58058ff8d3e507c3ef1f0e73a761263a7737b4e
Roman Brito <romanbrito1@gmail.com>
2018-06-18 15:32:37 -0500

    working on dropdown menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,8 @@ import {GC_USER_ID, GC_AUTH_TOKEN} from '../constants'
 import '../sass/Header.css'
 import styled from 'styled-components'
 import {Image} from 'cloudinary-react'
+import AngleDown from 'react-icons/lib/fa/angle-down'
+import AngleUp from 'react-icons/lib/fa/angle-up'
 
 // withRouter to get access to history
 const Header = withRouter((props) => (
@@ -18,11 +20,21 @@ const Header = withRouter((props) => (
           <li><NavLink dest='/' menuItem="Pricing"/></li>
           <li><NavLink dest='/' menuItem="About"/></li>
           {props.userId &&
-          <li><NavLink dest='/create' menuItem="Create Asset"/></li>}
-          {props.userId &&
-          <li><NavLink dest='/' menuItem="Logout"/></li>}
+          <li>
+            <a className="db pv3 ph3 f3 white link pointer">
+              Account
+                <AngleDown
+                  onClick={(e) => dropdownToggle(e)}
+                />
+            </a>
+            <ul className="dn-l absolute-l dropdown-bg">
+              <li className="list"><NavLink dest='/create' menuItem="Create Asset"/></li>
+              <li className="list"><NavLink dest='/' menuItem="Logout"/></li>
+            </ul>
+          </li>}
           {!props.userId && props.location.pathname !== '/login' ?
-          <li><NavLink dest='/login' menuItem="Login"/></li>:''}
+            <li><NavLink dest='/login' menuItem="Login"/></li> : ''}
+
         </Ul>
       </nav>
       <div className="dn db-l"><Image cloudName="spottermart" publicId="Logo2_qpq49e"/></div>
@@ -32,19 +44,20 @@ const Header = withRouter((props) => (
 
 const NavLink = (props) => (
   <Link className="db pv3 ph3 f3 white link" to={props.dest}
-  onClick={ () => {
-    // toggle menu
-    showMenu()
-    // logout
-    if (props.menuItem === 'Logout') {
-      localStorage.removeItem(GC_USER_ID)
-      localStorage.removeItem(GC_AUTH_TOKEN)
-      props.removeId()
-      props.history.push('/')
-    }}
-  }>
+        onClick={() => {
+          // toggle menu
+          showMenu()
+          // logout
+          if (props.menuItem === 'Logout') {
+            localStorage.removeItem(GC_USER_ID)
+            localStorage.removeItem(GC_AUTH_TOKEN)
+            props.removeId()
+            props.history.push('/')
+          }
+        }
+        }>
     {props.menuItem}
-    </Link>
+  </Link>
 )
 
 const MenuIcon = () => (
@@ -68,7 +81,19 @@ const Heading = styled.header.attrs({
 // toggle menu on small screens
 const showMenu = () => {
   const element = document.getElementById("main-nav");
-  (element.className === "dn flex-l items-center justify-between-l") ? element.className += " db": element.className = "dn flex-l items-center justify-between-l"
+  (element.className === "dn flex-l items-center justify-between-l") ? element.className += " db" : element.className = "dn flex-l items-center justify-between-l"
 }
+
+// drop down toggle menu for large screens
+const dropdownToggle = (e) => {
+  if (e.target.parentElement.nodeName === 'A') {
+    const element = e.target.parentElement.nextElementSibling;
+    (element.className === "dn-l absolute-l dropdown-bg") ? element.className += " db-l" : element.className="dn-l absolute-l dropdown-bg";
+    // in case you clicked directly into the svg object since parent would be g and svg
+  } else {
+    const element = e.target.parentElement.parentElement.parentElement.nextElementSibling;
+    (element.className === "dn-l absolute-l dropdown-bg") ? element.className += " db-l" : element.className="dn-l absolute-l dropdown-bg";
+  }
+};
 
 export default Header
