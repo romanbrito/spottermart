c262e9d4ac5c9cdf500a81409cd26b64592d77c2
Roman Brito <romanbrito1@gmail.com>
2018-06-26 22:29:15 -0500

    some refactor

--- src/components/containers/SearchPage.js
+++ src/components/containers/SearchPage.js
@@ -3,19 +3,20 @@ import {QueryRenderer, graphql} from 'react-relay'
 import environment from '../../Environment'
 
 const SearchPageQuery = graphql`
-  query SearchPageQuery($filter: AssetFilter!) {
-      viewer {
-          allAssets(filter: $filter){
-              edges {
-                  node {
-                      id
-                      businessName
-                  }
-              }   
-          }
-      }
-  }
+    query SearchPageQuery($filter: AssetFilter!) {
+        viewer {
+            allAssets(filter: $filter){
+                edges {
+                    node {
+                        id
+                        businessName
+                    }
+                }
+            }
+        }
+    }
 `
+
 class SearchPage extends Component {
 
   state = {
@@ -26,19 +27,25 @@ class SearchPage extends Component {
 
     return (
       <QueryRenderer
-      environment = {environment}
-      query = {SearchPageQuery}
-      variables = {{
-        filter: { OR: [{city_contains:this.state.filter},{businessName_contains:this.state.filter}]},
-      }}
-      render = {({error, props}) => {
-        if (error) {
-          return <div>{error.message}</div>
+        environment={environment}
+        query={SearchPageQuery}
+        variables={{
+          filter: {
+            OR: [
+              {city_contains: this.state.filter},
+              {businessName_contains: this.state.filter}
+            ]
+          },
+        }}
+        render={({error, props}) => {
+          if (error) {
+            return <div>{error.message}</div>
           } else if (props) {
-            return <ul>{props.viewer.allAssets.edges.map(edge => <li key={edge.node.id}>{edge.node.businessName}</li>)}</ul>
+            return <ul>{props.viewer.allAssets.edges.map(edge => <li
+              key={edge.node.id}>{edge.node.businessName}</li>)}</ul>
           }
           return <div>Loading</div>
-      }}
+        }}
       />
     )
   }
