164dbe77c3df57b341e9b72fa8e377e9fea7d57b
Roman Brito <romanbrito1@gmail.com>
2018-07-09 18:52:43 -0500

    styled components to avoid clutter

---
+++ src/components/ui/StyledCreateAsset.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+export const Main = styled.main.attrs({
+  className: "list pl0 mt0 measure center ml5-l measure-narrow-l",
+})``
+
+export const Section = styled.section.attrs({
+  className: "flex lh-copy pa3 ph0-l bb b--black-10"
+})``
+
+export const Article = styled.article.attrs({
+  className: "flex-auto"
+})``
+
+export const SubTitle = styled.h2.attrs({
+  className: "pointer w-100 accordion green"
+})``
