6041d5107b35cb114ab3af120cf3c10276375cb7
Roman Brito <romanbrito1@gmail.com>
2018-07-12 17:16:11 -0500

    wiring send message

--- src/components/containers/CreateMessage.js
+++ src/components/containers/CreateMessage.js
@@ -4,15 +4,31 @@ import CreateMessageUi from '../ui/CreateMessage'
 class CreateMessage extends Component {
 
   state = {
-    text: ''
+    messageText: ''
   }
 
   render () {
 
     return (
-      <CreateMessageUi/>
+      <CreateMessageUi onChange={this._onChange} messageText={this.state.messageText}/>
     )
   }
+
+  // functions for change of input
+  _onChange = event => {
+    const {name, value} = event.target
+    this._newState(name,value)
+  }
+
+  _newState = (element, newElement) => {
+    const newState = Object.keys(this.state).reduce((prev, curr) => {
+      if (curr === element) prev[curr] = newElement
+      else prev[curr] = this.state[curr]
+      return prev
+    },  {})
+    this.setState(newState)
+  }
+
 
 }
 
--- src/components/ui/CreateMessage.js
+++ src/components/ui/CreateMessage.js
@@ -5,11 +5,11 @@ const CreateMessage = withRouter((props) =>
   <article className="center mw5 mw6-ns br3 hidden ba b--black-10 mv4">
     <h1 className="f4 bg-near-white br3 br--top black-60 mv0 pv2 ph3">Message</h1>
     <div className="pa3 bt b--black-10">
-      <textarea className="f6 f5-ns lh-copy measure">
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-        tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-        vero eos et accusam et justo duo dolores et ea rebum.
-      </textarea>
+      <textarea
+        name="messageText"
+        value={props.messageText}
+        onChange={e => props.onChange(e)}
+        className="f6 f5-ns lh-copy measure"/>
     </div>
     <section>
       <button
