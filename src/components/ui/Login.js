e9b92f1fc1645f47df035c0ca3e1fcc870783e20
roman <romanbrito1@gmail.com>
2018-07-15 16:20:49 -0500

    style sign in

--- src/components/ui/Login.js
+++ src/components/ui/Login.js
@@ -1,45 +1,61 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {
+  Main,
+  Form,
+  SignUpField,
+  Legend,
+  Email,
+  EmailLabel,
+  EmailInput,
+  Password,
+  PasswordLabel,
+  PasswordInput,
+  RememberMeLabel,
+  Submit,
+  SubmitBtn,
+  SignUP,
+  SignUpBtn,
+} from './StyledLogin'
 
 const Login = (props) =>
-  <main className="pa4 black-80">
-    <form className="measure center">
-      <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-        <legend className="f4 fw6 ph0 mh0">Sign In</legend>
-        <div className="mt3">
-          <label className="db fw6 lh-copy f6" htmlFor="email">Email</label>
-          <input
-            className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+  <Main>
+    <Form>
+      <SignUpField>
+        <Legend>Sign In</Legend>
+        <Email>
+          <EmailLabel htmlFor="email">Email</EmailLabel>
+          <EmailInput
             type="email"
             name="email" id="email"
             onChange={e => props.onChange(e)}
           />
-        </div>
-        <div className="mv3">
-          <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-          <input
-            className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+        </Email>
+        <Password>
+          <PasswordLabel htmlFor="password">Password</PasswordLabel>
+          <PasswordInput
             type="password"
             name="password"
             id="password"
             onChange={e => props.onChange(e)}
           />
-        </div>
-        <label className="pa0 ma0 lh-copy f6 pointer"><input type="checkbox"/> Remember me</label>
-      </fieldset>
-      <div className="ph3">
-        <p
-          className="f6 link dim ph3 pv2 mb2 dib white bg-black pointer"
+        </Password>
+        <RememberMeLabel>
+          <input type="checkbox"/> Remember me
+        </RememberMeLabel>
+      </SignUpField>
+      <Submit>
+        <SubmitBtn
           onClick={() => props.confirm()}
         >
           Submit
-        </p>
-      </div>
-      <div className="lh-copy mt3">
-        <Link to='/signup'><p className="f6 link dim black db pointer">Sign up</p></Link>
-        <a href="#0" className="f6 link dim black db">Forgot your password?</a>
-      </div>
-    </form>
-  </main>
+        </SubmitBtn>
+      </Submit>
+      <SignUP>
+        <Link to='/signup'><SignUpBtn>Sign up</SignUpBtn></Link>
+        <Link to='/signup'><SignUpBtn>Forgot your password?</SignUpBtn></Link>
+      </SignUP>
+    </Form>
+  </Main>
 
 export default Login
---
+++ src/components/ui/StyledLogin.js
@@ -0,0 +1,62 @@
+import styled from 'styled-components'
+
+export const Main = styled.main.attrs({
+  className: "pa4 black-80"
+})``
+
+export const Form = styled.form.attrs({
+  className: "measure center"
+})``
+
+export const SignUpField = styled.fieldset.attrs({
+  className: "ba b--transparent ph0 mh0",
+  id:"sign_up"
+})``
+
+export const Legend = styled.legend.attrs({
+  className: "f4 fw6 ph0 mh0"
+})``
+
+export const Email =styled.div.attrs({
+  className: "mt3"
+})``
+
+export const EmailLabel = styled.div.attrs({
+  className: "db fw6 1h-copy f6"
+})``
+
+export const EmailInput = styled.input.attrs({
+  className: "pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+})``
+
+export const Password = styled.div.attrs({
+  className: "mv3"
+})``
+
+export const PasswordLabel = styled.label.attrs({
+  className: "db fw6 lh-copy f6"
+})``
+
+export const PasswordInput = styled.input.attrs({
+  className: "b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+})``
+
+export const RememberMeLabel = styled.label.attrs({
+  className: "pa0 ma0 lh-copy f6 pointer"
+})``
+
+export const Submit = styled.div.attrs({
+  className: "ph3"
+})``
+
+export const SubmitBtn = styled.button.attrs({
+  className: "f6 link dim ph3 pv2 mb2 dib white bg-black pointer"
+})``
+
+export const SignUP = styled.div.attrs({
+  className: "lh-copy mt3"
+})``
+
+export const SignUpBtn = styled.p.attrs({
+  className: "f6 link dim black db pointer"
+})``
