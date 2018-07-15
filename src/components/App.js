9c8cf82d97b062836204de9c145832b8a9425f7c
roman <romanbrito1@gmail.com>
2018-07-14 22:07:27 -0500

    adding userName

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,30 @@
 import React, {Component} from 'react'
 import {GC_AUTH_TOKEN, GC_USER_ID} from '../constants'
+import {fetchQuery} from '../Environment'
 import Header from './Header'
 import Main from './Main'
 
 class App extends Component {
   state = {
-    userId: ''
+    userId: '',
+    userName:''
   }
 
   componentWillMount() {
+    const userId = localStorage.getItem(GC_USER_ID)
+
     this._getId()
+
+    if (userId) {
+      this._getUserName(userId)
+    }
+
   }
 
   render() {
     return (
       <div className="App">
-        <Header userId={this.state.userId} removeId={this._removeId}/>
+        <Header userId={this.state.userId} removeId={this._removeId} userName={this.state.userName}/>
         <Main getId={this._getId}/>
       </div>
     );
@@ -27,6 +36,15 @@ class App extends Component {
     })
   }
 
+  _getUserName = (userId) => {
+
+    _userInfo(userId).then(
+      result => this.setState({userName: result.data.viewer.User.name})
+    )
+
+  }
+
+
   _removeId = () => {
     localStorage.removeItem(GC_USER_ID)
     localStorage.removeItem(GC_AUTH_TOKEN)
@@ -34,4 +52,21 @@ class App extends Component {
   }
 }
 
-export default App;
+export default App
+
+// query to get username
+const _userInfo = async (userId) => {
+  const userQueryText = `
+    query GetUserName($userId: ID!) {
+      viewer {
+        User(id:$userId){
+          name
+        }
+      }
+    }
+    `
+  const userInfoQuery = {text: userQueryText}
+
+  const result = await fetchQuery(userInfoQuery,{userId})
+  return result
+}
